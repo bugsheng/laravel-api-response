061a8f531e20b8128dc59aceb5fb9249d2261c31
盛林 <shenglin@hptown.cn>
2024-02-28 11:07:19 +0800

    优化调整

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,7 +5,9 @@ namespace BugSheng\Laravel\ApiResponse;
 
 use BugSheng\Laravel\ApiResponse\Contracts\JsonResponseContract;
 use Illuminate\Foundation\Application as LaravelApplication;
-use Illuminate\Support\Facades\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 use Laravel\Lumen\Application as LumenApplication;
 
@@ -16,40 +18,44 @@ class ServiceProvider extends LaravelServiceProvider
     {
         Response::macro('success', function ($data = [], $message = '', $headers = [], $options = 0) {
             $sendData = [
+                'request_id' => Request::instance()->requestId ?? '',
                 'code'    => 0,
                 'message' => $message,
-                'data'    => $data
+                'data'    => $data ?: new \stdClass()
             ];
-            return Response::json($sendData, 200, $headers, $options);
+            return new JsonResponse($sendData, 200, $headers, $options);
         });
 
         Response::macro('fail',
             function ($message = '', $data = [], $serverCode = 400, $httpCode = 200, $headers = [], $options = 0) {
                 $sendData = [
+                    'request_id' => Request::instance()->requestId ?? '',
                     'code'    => $serverCode,
                     'message' => $message,
-                    'data'    => $data
+                    'data'    => $data ?: new \stdClass()
                 ];
-                return Response::json($sendData, $httpCode, $headers, $options);
+                return new JsonResponse($sendData, $httpCode, $headers, $options);
             });
 
         Response::macro('msg', function ($message = '', $headers = [], $options = 0) {
             $sendData = [
+                'request_id' => Request::instance()->requestId ?? '',
                 'code'    => 0,
                 'message' => $message,
-                'data'    => []
+                'data'    => new \stdClass()
             ];
-            return Response::json($sendData, 200, $headers, $options);
+            return new JsonResponse($sendData, 200, $headers, $options);
         });
 
         Response::macro('errMsg',
             function ($message = '', $serverCode = 400, $httpCode = 200, $headers = [], $options = 0) {
                 $sendData = [
+                    'request_id' => Request::instance()->requestId ?? '',
                     'code'    => $serverCode,
                     'message' => $message,
-                    'data'    => []
+                    'data'    => new \stdClass()
                 ];
-                return Response::json($sendData, $httpCode, $headers, $options);
+                return new JsonResponse($sendData, $httpCode, $headers, $options);
             });
     }
 
